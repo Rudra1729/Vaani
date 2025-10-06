d9ed6d5a1ef0ed448f1085ddbf9f1eda487e0e52
Rudra1729 <rudrapatel222444@gmail.com>
2025-10-06 15:20:58 -0400

    Remove sensitive files from tracking

--- PromptEngineering/API_KEY.py
+++
@@ -1,5 +0,0 @@
-API_KEY = 'AIzaSyCfyyPp_r9NyoKafxIvIEXj4RvNzwybpWk'
-ELEVENLABS_API_KEY = 'sk_67b4c1766e63aabcaf95af001c49b78bae5a3edbd413e29e'  
-# Replace with actual API key
-
-#'sk_36809648edf1d9097455258e07e2c1e3de889c5bc2b90caa'
